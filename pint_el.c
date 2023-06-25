f5d4c0f3a828da39b0ce658293359d1b0f47cb5a
Abdul Rahman <abdulnsamba@gmail.com>
2023-06-25 16:21:00 +0300

    pint

--- pint_el.c
+++ pint_el.c
@@ -17,7 +17,7 @@ void pint(stack_t **stack, unsigned int line_number)
 
 	if (temp == NULL)
 	{
-		fprintf(stderr, "L %d: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else
