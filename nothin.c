aa604c283de17536f9df5dfc8e3f9558df42c40c
Abdul Rahman <abdulnsamba@gmail.com>
2023-06-25 21:57:08 +0300

    non

---
+++ nothin.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * nop - This function does nothin
+ * @stack: Stack of elements
+ * @line_number: line of the file
+ *
+ * Description: does nothin
+ * Return: Nothing
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
